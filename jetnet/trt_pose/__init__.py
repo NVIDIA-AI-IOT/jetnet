06a4a588561be7763196505b164815cfc61ffd09
John Welsh <jaybdub@users.noreply.github.com>
2022-09-20 23:24:08 +0000

    remove unused trt pose

--- jetnet/trt_pose/__init__.py
+++ jetnet/trt_pose/__init__.py
@@ -1,5 +1,3 @@
 from .body import *
 from .hand import *
 from .trt_pose import *
-# from .trt_pose_model_config import TRTPoseModelConfig
-# from .trt_pose_trt_model_config import TRTPoseTRTModelConfig
