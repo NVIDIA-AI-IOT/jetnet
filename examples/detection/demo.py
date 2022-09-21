b50293c47b050bc9399ef9a22a045337b2663a11
John Welsh <jaybdub@users.noreply.github.com>
2022-09-21 19:09:29 +0000

    add license headers

--- examples/detection/demo.py
+++ examples/detection/demo.py
@@ -1,11 +1,25 @@
+# SPDX-FileCopyrightText: Copyright (c) <year> NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from jetnet.yolox import YOLOX_TINY_TRT_FP16
-from jetnet.demo.detection_demo import DetectionDemoConfig
+from jetnet.demo.detection import DetectionDemo
 
 
-config = DetectionDemoConfig(
-    model_config=YOLOX_TINY_TRT_FP16
+demo = DetectionDemo(
+    model=YOLOX_TINY_TRT_FP16
 )
-
-demo = config.build()
 
 demo.run()
