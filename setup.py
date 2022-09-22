22c21454736b0d1a0b4ee78de144eca64c7af4ed
John Welsh <jaybdub@users.noreply.github.com>
2022-09-22 18:50:07 +0000

    description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="jetnet",
     version="0.0.0",
-    description="Easy to use neural networks on NVIDIA Jetson",
+    description="Easy to use neural networks for NVIDIA Jetson (and desktop too!)",
     packages=find_packages(),
     entry_points={"console_scripts": ["jetnet = jetnet.__main__:main"]},
     install_requires=["pydantic"]
